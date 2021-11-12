8afec65a0ae1c3fd2ef8d2b1519e7d33b7bf44b4
Matthew LaRocca <mlarocca.cpp@gmail.com>
2021-11-11 17:58:46 -0800

    wip

--- src/moving_platform.c
+++ src/moving_platform.c
@@ -1,35 +1,41 @@
 #include "moving_platform.h"
 
-void moving_platform_init(MovingPlatform* platform, fw64Node* node, Vec3* movement_vec, float dist, float speed, Player* player) {
-    platform->node = node;
-    platform->movement_vec = *movement_vec;
-    platform->max_dist = dist;
-    platform->player = player;
+void moving_platform_init(MovingPlatform* platform, fw64Node* platform_node, fw64Node* target_node, float speed, Player* player) {
+    platform->platform_node = platform_node;
+    platform->target_node = target_node;
     platform->speed = speed;
+    platform->player = player;
 
-    platform->traveled_dist = 0.0f;
+    platform->start_pos = platform_node->transform.position;
+    platform->target_pos = target_node->transform.position;
+    platform->total_dist = vec3_distance(&platform->start_pos, & platform->target_pos);
+    platform->current_dist = 0.0f;
 }
 
+
 void moving_platform_update(MovingPlatform* platform, float time_delta) {
-    platform->traveled_dist += platform->speed * time_delta;
-    Vec3 previous_pos = platform->node->transform.position;
+    platform->current_dist += platform->speed * time_delta;
+    Vec3 previous_pos = platform->platform_node->transform.position;
 
     // if platform has moved to its max, need to switch directions
-    if (platform->traveled_dist >= platform->max_dist) {
-        vec3_add_and_scale(&platform->node->transform.position, &platform->node->transform.position, &platform->movement_vec, platform->traveled_dist - platform->max_dist);
-
-        vec3_negate(&platform->movement_vec);
-        platform->traveled_dist = 0.0f;
+    if (platform->current_dist >= platform->total_dist) {
+        platform->platform_node->transform.position = platform->target_pos;
+        
+        platform->current_dist = 0.0f;
+        Vec3 temp = platform->start_pos;
+        platform->start_pos = platform->target_pos;
+        platform->target_pos = temp;
     }
     else {
-        vec3_add_and_scale(&platform->node->transform.position, &platform->node->transform.position, &platform->movement_vec, platform->speed * time_delta);
+        float t = platform->current_dist / platform->total_dist;
+        vec3_smoothstep(&platform->platform_node->transform.position, &platform->start_pos, &platform->target_pos, t);
     }
 
-    fw64_node_update(platform->node);
+    fw64_node_update(platform->platform_node);
 
-    if (platform->player->ground_node == platform->node) {
+    if (platform->player->ground_node == platform->platform_node) {
         Vec3 delta;
-        vec3_subtract(&delta, &platform->node->transform.position, &previous_pos);
+        vec3_subtract(&delta, &platform->platform_node->transform.position, &previous_pos);
 
         vec3_add(&platform->player->node.transform.position, &platform->player->node.transform.position, &delta);
         platform->player->previous_position = platform->player->node.transform.position;
--- src/moving_platform.h
+++ src/moving_platform.h
@@ -4,27 +4,22 @@
 #include "player.h"
 
 typedef struct {
-    fw64Node* node;
-    
-    /** current direction of travel */
-    Vec3 movement_vec;
-
-    /** how far the platform will travel before reversing direction */
-    float max_dist;
-
-    /** speed at which the platform will travel */
+    fw64Node* platform_node;
+    fw64Node* target_node;
     float speed;
     Player* player;
-
-    /** how far the platform has traveled since last switching directions*/
-    float traveled_dist;
+    
+    Vec3 start_pos;
+    Vec3 target_pos;
+    float total_dist;
+    float current_dist;
 } MovingPlatform;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void moving_platform_init(MovingPlatform* platform, fw64Node* node, Vec3* movement_vec, float dist, float speed, Player* player);
+void moving_platform_init(MovingPlatform* platform, fw64Node* platform_node, fw64Node* target_node, float speed, Player* player);
 void moving_platform_update(MovingPlatform* platform, float time_delta);
 
 #ifdef __cplusplus
