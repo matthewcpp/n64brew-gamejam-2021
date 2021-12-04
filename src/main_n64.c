e67bfb8b071dd01074e9b7ec054a6ba1eba0f900
kivan117 <12374646+kivan117@users.noreply.github.com>
2021-12-04 02:06:40 +0000

    add PAL and MPAL support to main

--- src/main_n64.c
+++ src/main_n64.c
@@ -8,6 +8,10 @@
 Game game;
 fw64Engine engine;
 
+#if defined(TV_MODE_PAL)
+static void callback_prenmi();
+#endif
+
 void nusys_game_tick(int pendingGfx) {
     if (pendingGfx < 1) {
         fw64_n64_engine_update(&engine);
@@ -17,8 +21,25 @@ void nusys_game_tick(int pendingGfx) {
 }
 
 void mainproc(void) {
+
+    #if defined(TV_MODE_PAL)
+        osViSetMode(&osViModeTable[OS_VI_FPAL_LAN1]);
+        osViSetYScale(0.833);
+        nuPreNMIFuncSet((NUScPreNMIFunc)callback_prenmi);
+    #elif defined(TV_MODE_MPAL)
+        osViSetMode(&osViModeTable[OS_VI_MPAL_LAN1]);
+    #endif       
+
     fw64_n64_engine_init(&engine, FW64_ASSET_COUNT);
     game_init(&game, &engine);
     nuGfxFuncSet((NUGfxFunc)nusys_game_tick);
     nuGfxDisplayOn();
 }
+
+#if defined(TV_MODE_PAL)
+void callback_prenmi()
+{
+    nuGfxDisplayOff();
+    osViSetYScale(1);
+}
+#endif
